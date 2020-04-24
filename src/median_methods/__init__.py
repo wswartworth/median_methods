b7648f7ff798d5bf60e2fc399bbdbc64a7673b33
William Swartworth <WilliamSwartworth@Williams-MacBook-Pro.local>
2020-04-23 22:19:49 -0700

    Initial commit

---
+++ src/median_methods/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for Median Methods."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+__author__ = "William Swartworth"
+__email__ = "wswartworth@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, William Swartworth"
+
+
+from .example import Example  # noqa: F401
